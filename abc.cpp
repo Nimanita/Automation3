0211a96d4ad718cff35bf14c6d3131af36581058
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-09-10 20:43:37 +0530

    Update abc.cpp

--- abc.cpp
+++ abc.cpp
@@ -4,6 +4,13 @@ int main(){
  int phone_number = "+91-324897324";
   char* user_name = "tom phrises"
   int ACCOUNT ID = "EG637U4"
-  
+  const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="serger";
+const stripeAcct = 12123;
+const username = "asfweferg.com";
+const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="serger";
+const stripeAcct = 12123;
+const username = "asfweferg.com";
     return 0;
 }
